59e909571685125c6b2e50d19ccc0c3d5a94cf65
ghailen <benachourghailen@gmail.com>
2021-10-31 12:43:53 +0100

    update validation file

---
+++ train_test/validation.py
@@ -0,0 +1,25 @@
+from torch.nn import CrossEntropyLoss
+import torch
+
+def valid(args, model, test_loader, device):
+    print("***VALIDATION***")
+    print("---------------")
+    model.eval()
+    correct = 0
+    total_loss = 0
+    loss_fct = CrossEntropyLoss()
+    for step, batch in enumerate(test_loader):
+        batch = tuple(t.to(device) for t in batch)
+        x, y = batch
+        with torch.no_grad():
+            logits = model(x)
+
+            total_loss += loss_fct(logits, y)
+            
+            preds = torch.argmax(logits, dim=-1)
+
+            correct += (preds == y).float().sum()
+
+    accuracy = 100 * correct / (step * args.batch_size)
+    total_loss = total_loss / step  
+    return accuracy, total_loss
