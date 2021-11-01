5303cde202967fcc3b837f77c9c4768836fb829d
ghailen <benachourghailen@gmail.com>
2021-11-01 08:58:50 +0100

    load checkpoint

--- utils.py
+++ utils.py
@@ -6,3 +6,8 @@ def save_checkpoint(state, args):
     torch.save(state, os.path.join(args.output_dir, "%s_checkpoint.ckpt" % args.name))
 
 
+def load_checkpoint(args, map_location=None):
+    ckpt_path = os.path.join(args.output_dir, "%s_checkpoint.ckpt" % args.name)
+    ckpt = torch.load(ckpt_path, map_location=map_location)
+    print(' [*] Loading checkpoint from %s succeed!' % ckpt_path)
+    return ckpt
