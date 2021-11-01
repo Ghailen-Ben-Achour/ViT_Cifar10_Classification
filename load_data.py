141fb1bff33cdd9f8c41fd618fc2957dc48a646e
ghailen <benachourghailen@gmail.com>
2021-11-01 09:00:13 +0100

    load cifar10

---
+++ load_data.py
@@ -0,0 +1,42 @@
+import torch
+
+from torchvision import transforms, datasets
+from torch.utils.data import DataLoader, RandomSampler, DistributedSampler, SequentialSampler
+
+
+
+def get_loader(args):
+
+    transform_train = transforms.Compose([
+        transforms.RandomResizedCrop((args.img_size, args.img_size), scale=(0.05, 1.0)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
+    ])
+    transform_test = transforms.Compose([
+        transforms.Resize((args.img_size, args.img_size)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
+    ])
+
+    
+    trainset = datasets.CIFAR10(root="./data",
+                                    train=True,
+                                    download=True,
+                                    transform=transform_train)
+    testset = datasets.CIFAR10(root="./data",
+                                   train=False,
+                                   download=True,
+                                   transform=transform_test) 
+
+ 
+    
+    train_loader = DataLoader(trainset,
+                              batch_size=args.batch_size,
+                              num_workers=2,
+                              pin_memory=True)
+    test_loader = DataLoader(testset,
+                             batch_size=args.batch_size,
+                             num_workers=2,
+                             pin_memory=True)
+    return train_loader, test_loader
+
